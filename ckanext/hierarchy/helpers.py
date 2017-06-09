dc12104393995338dd6d86b788d17971a3604f09
Harald von Waldow <harald.vonwaldow@eawag.ch>
2017-06-09 18:58:10 +0200

    group_tree_parents returns id & display_name

--- ckanext/hierarchy/helpers.py
+++ ckanext/hierarchy/helpers.py
@@ -1,9 +1,7 @@
 import ckan.plugins as p
 
-
 def group_tree(type_='organization'):
     return p.toolkit.get_action('group_tree')({}, {'type': type_})
-
 
 def group_tree_section(id_, type_='organization'):
     return p.toolkit.get_action('group_tree_section')(
@@ -13,7 +11,9 @@ def group_tree_parents(id_, type_='organization'):
      if (tree_node['groups']):
          parent_id = tree_node['groups'][0]['name']
          parent_node =  p.toolkit.get_action('organization_show')({},{'id':parent_id})
-         return group_tree_parents(parent_id) + [parent_node]
+         parent_nodename =  {'displayname': parent_node.get('shortname', parent_node.get('title')),
+                             'id': parent_id}
+         return group_tree_parents(parent_id) + [parent_nodename]
      else:
          return []
 
