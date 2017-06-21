e166799d0f6019b239707319443cb1d1d335f298
Harald von Waldow <harald.vonwaldow@eawag.ch>
2017-06-21 15:18:14 +0200

    plugin - WIP

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -2,6 +2,9 @@ import ckan.plugins as p
 from ckanext.hierarchy.logic import action
 from ckanext.hierarchy import helpers
 from ckan.lib.plugins import DefaultOrganizationForm
+import logging
+
+log = logging.getLogger(__name__)
 
 # This plugin is designed to work only these versions of CKAN
 p.toolkit.check_ckan_version(min_version='2.0')
@@ -12,6 +15,7 @@ class HierarchyDisplay(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IActions, inherit=True)
     p.implements(p.ITemplateHelpers, inherit=True)
+    p.implements(p.IPackageController, inherit=True)
 
     # IConfigurer
 
@@ -35,6 +39,59 @@ class HierarchyDisplay(p.SingletonPlugin):
                 'get_allowable_parent_groups': helpers.get_allowable_parent_groups,
                 'render_tree': helpers.render_tree
                 }
+
+    # IPackageController
+    # Modify the search query to include the datasets from
+    # the children organizations in the result list
+    # HvW: Do this always
+    def before_search(self, search_params):
+        print('------------------- IN BEFORE_SEARCH -----------------------')
+        
+
+        ''' If include children selected the query string is modified '''
+
+        def _tokenize_search(queryfield):
+            def repspace(st):
+                # deal with escaped brackets
+                # issue if st tarts with ( ? 
+                pat = re.compile(r'(\([^)]*\))+')
+                brackets = re.finditer(pat, st)
+                splitted = re.split(pat, st)
+                repstrings = []
+                for b in brackets:
+                    repstrings.append(re.sub(r' +', '__SPACE__',b.group()))
+                for k, i in enumerate(range(1,len(splitted),2)):
+                    splitted[i] = repstrings[k] 
+                return ''.join(splitted)
+
+            def splitspaces(st):
+                st = re.sub(": +", ":", st)
+                ## split querystring at spaces if space doesn't occur in quotes
+                ## http://stackoverflow.com/a/2787064
+                # deal with escaped quotqtion marks
+                # implement this: https://stackoverflow.com/a/2787979/6930916
+                pat = re.compile(r'''((?:[^ "']|"[^"]*"|'[^']*')+)''')
+                splitspaces = pat.split(st)[1::2]
+                splitspaces = [el.replace('__SPACE__',' ') for el in splitspaces]
+                return splitspaces
+
+            splitquery = splitspaces(repspace(search_params.get(queryfield, '')))
+            querylist = [e.split(':', 1) for e in splitquery]
+            return querylist
+        
+        def _assemble_query(q_list):
+            print q_list
+            q_string = ' '.join([':'.join(el) for el in q_list])
+            return q_string
+            
+        # q_list = _tokenize_search('q')
+        # search_params['q'] = _assemble_query(q_list)
+        # fq_list = _tokenize_search('fq')
+        # search_params['fq'] = _assemble_query(fq_list)
+        print('--------------SEARCH PARAMS ----------------------------------------------')
+        log.debug(search_params)
+        print('------------------------------------------------------------')
+        return search_params
 
 
 class HierarchyForm(p.SingletonPlugin, DefaultOrganizationForm):
